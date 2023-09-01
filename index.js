89349104c760e0afe25ae2bce0160c02ded02c96
Shamil Kh <shamilist90@gmail.com>
2023-09-01 10:49:23 +0300

    h

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const bot = require(`node-telegram-bot-api`)
+const TelegramApi = require("node-telegram-bot-api");
 
 const token = "6123823998:AAFeNGspj_NK7yjAkV7FeemlKMlI5xYanxw";
+
+const bot = new TelegramApi(token,  { polling: true});
+
+bot.on('message', async msg => {
+    const text = msg.text;
+    const chatId = msg.chat.id;
+
+    if (text === "/start") {
+    await bot.sendSticker(chatId, 'https://stickerswiki.ams3.cdn.digitaloceanspaces.com/MemeS1ick3r/429752.512.mp4');
+    await bot.sendMessage(chatId, `Добро пожаловать в бот!`)
+    }
+
+    if (text === "/info" ) {
+        await bot.sendMessage(chatId, `Тебя зовут ${msg.from.username}`)
+    }
+});
