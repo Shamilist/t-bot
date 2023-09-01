5c057872bc43e52bc624cb94372523c8f139ba61
Shamil Kh <shamilist90@gmail.com>
2023-09-01 17:26:33 +0300

    add game

--- index.js
+++ index.js
@@ -4,16 +4,38 @@ const token = "6123823998:AAFeNGspj_NK7yjAkV7FeemlKMlI5xYanxw";
 
 const bot = new TelegramApi(token,  { polling: true});
 
+const chats = {};
+
+
+
+const start = () => {
+    bot.setMyCommands([
+    { command: '/start', description: 'Начальное приветствие'},
+    { command: '/info', description: 'Информация о пользователе'}
+])
+
 bot.on('message', async msg => {
     const text = msg.text;
     const chatId = msg.chat.id;
 
     if (text === "/start") {
     await bot.sendSticker(chatId, 'https://stickerswiki.ams3.cdn.digitaloceanspaces.com/MemeS1ick3r/429752.512.mp4');
-    await bot.sendMessage(chatId, `Добро пожаловать в бот!`)
+    return bot.sendMessage(chatId, `Добро пожаловать в бот!`)
     }
 
     if (text === "/info" ) {
-        await bot.sendMessage(chatId, `Тебя зовут ${msg.from.username}`)
+        return bot.sendMessage(chatId, `Тебя зовут ${msg.from.username}`)
     }
+
+    if (text === "/game" ) {
+        await bot.sendMessage(chatId, `Я загадал число от 0 до 9, ты должен угадать его :)`)
+        const randomNumber = Math.floor(Math.random() * 10);
+        chats[chatId] = randomNumber;
+        return bot.sendMessage(chatId, 'Начинай отгадывать!')
+    }
+
+    return bot.sendMessage(chatId, "Я тебя не понимаю, попробуй ещё раз!");
 });
+}
+
+start();
